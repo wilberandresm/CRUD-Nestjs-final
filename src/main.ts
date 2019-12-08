7d564d46842d448c58003981c79a14487c469fd6
wilberandresm <wilberandresm@hotmail.com>
2019-12-08 14:15:56 -0500

    v1.1
    Habilitar CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = 3000;
+  app.enableCors();
   await app.listen(PORT, () => console.log(`Server is live at http://localhost:${PORT}`));
 }
 bootstrap();
